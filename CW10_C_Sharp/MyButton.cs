572aa7e056d416e8961e131ff98a19b3175019df
Pavlo <pkhasanov@gmail.com>
2023-10-11 19:55:41 +0300

    Add project files.

---
+++ CW10_C_Sharp/MyButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW10_C_Sharp
+{
+    internal class MyButton
+    {
+        public event Action MyClickButton;
+        public string Color {  get; set; }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public string Text { get; set; }
+
+        public void ClickOnButton(bool isClick)
+        {
+            if(isClick)
+            {
+                MyClickButton.Invoke();
+            }
+        }
+    }
+}
---
+++ CW10_C_Sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW10_C_Sharp
+{
+    internal class Program
+    {
+        public static void ClickOnButton()
+        {
+            Console.WriteLine($"Кнопка нажата {DateTime.Now.ToString()}");
+        }
+        static void Main(string[] args)
+        {
+            MyButton start = new MyButton() { Color = "White", Height = 5.5, Width = 10.5, Text = "START" };
+            start.MyClickButton += ClickOnButton;
+            start.ClickOnButton(true);
+            Console.ReadLine();
+
+        }
+    }
+}
